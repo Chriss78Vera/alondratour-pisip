51f3ce781be30d68b92b293be1ac6be14b8b45a4
Jordan Paillacho <jordan270398@gmail.com>
2026-01-26 02:56:32 -0500

    Actualizar Pasajeros Reservas

--- src/main/java/com/pisip/alondrabackend/infraestructura/configuracion/ConfiguracionGeneral.java
+++ src/main/java/com/pisip/alondrabackend/infraestructura/configuracion/ConfiguracionGeneral.java
@@ -4,16 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.pisip.alondrabackend.aplicacion.casosuso.entradas.IHotelesUseCase;
+import com.pisip.alondrabackend.aplicacion.casosuso.entradas.IPasajerosUseCase;
+import com.pisip.alondrabackend.aplicacion.casosuso.entradas.IReservasUseCase;
 import com.pisip.alondrabackend.aplicacion.casosuso.entradas.IVuelosUseCase;
 import com.pisip.alondrabackend.aplicacion.casosuso.impl.HotelesUseCaseImpl;
+import com.pisip.alondrabackend.aplicacion.casosuso.impl.PasajerosUseCaseImpl;
+import com.pisip.alondrabackend.aplicacion.casosuso.impl.ReservasUseCaseImpl;
 import com.pisip.alondrabackend.aplicacion.casosuso.impl.VuelosUseCaseImpl;
 import com.pisip.alondrabackend.dominio.repositorios.IHotelesRepositorio;
+import com.pisip.alondrabackend.dominio.repositorios.IPasajerosRepositorio;
+import com.pisip.alondrabackend.dominio.repositorios.IReservasRepositorio;
 import com.pisip.alondrabackend.dominio.repositorios.IVuelosRepositorio;
 import com.pisip.alondrabackend.infraestructura.persistencia.adaptadores.HotelesRepositorioImpl;
+import com.pisip.alondrabackend.infraestructura.persistencia.adaptadores.PasajerosRepositorioImpl;
+import com.pisip.alondrabackend.infraestructura.persistencia.adaptadores.ReservasRepositorioImpl;
 import com.pisip.alondrabackend.infraestructura.persistencia.adaptadores.VuelosRepositorioImpl;
 import com.pisip.alondrabackend.infraestructura.persistencia.mapeadores.IHotelesJpaMapper;
+import com.pisip.alondrabackend.infraestructura.persistencia.mapeadores.IPasajerosJpaMapper;
+import com.pisip.alondrabackend.infraestructura.persistencia.mapeadores.IReservasJpaMapper;
 import com.pisip.alondrabackend.infraestructura.persistencia.mapeadores.IVuelosJpaMapper;
 import com.pisip.alondrabackend.infraestructura.repositorios.IHotelesJpaRepositorio;
+import com.pisip.alondrabackend.infraestructura.repositorios.IPasajerosJpaRepositorio;
+import com.pisip.alondrabackend.infraestructura.repositorios.IReservasJpaRepositorio;
 import com.pisip.alondrabackend.infraestructura.repositorios.IVuelosJpaRepositorio;
 
 @Configuration
@@ -37,4 +49,27 @@ public class ConfiguracionGeneral {
 	IVuelosUseCase VuelosUseCase(IVuelosRepositorio repositorio) {
 		return new VuelosUseCaseImpl(repositorio);
 	}
+	
+	
+	@Bean
+	IPasajerosRepositorio PasajerosRepositorio(IPasajerosJpaRepositorio jpaRepository, IPasajerosJpaMapper mapper) {
+		return new PasajerosRepositorioImpl(jpaRepository, mapper);
+	}
+	
+	@Bean
+	IPasajerosUseCase PasajerosUseCase(IPasajerosRepositorio repositorio) {
+		return new PasajerosUseCaseImpl(repositorio);
+	}
+	
+	@Bean
+	IReservasRepositorio ReservasRepositorio(IReservasJpaRepositorio jpaRepository, IReservasJpaMapper mapper) {
+		return new ReservasRepositorioImpl(jpaRepository, mapper);
+	}
+	
+	@Bean
+	IReservasUseCase ReservasUseCase(IReservasRepositorio repositorio) {
+		return new ReservasUseCaseImpl(repositorio);
+	}
+	
+	
 }
